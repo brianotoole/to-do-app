f795e5172baecaafeff40216e14eabe008601af8
Brian OToole <botoole518@gmail.com>
2017-04-16 17:57:06 +0000

    separated routes into routes file

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 var express = require('express');
-var bodyParser= require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
-var exphbs = require('express-handlebars');
 var path = require('path');
-var app = express();
+var bodyParser= require('body-parser');
 
-// Connect to mongo db
-var db;
-MongoClient.connect('mongodb://dev:35Xsi6C8*136@ds161640.mlab.com:61640/todolist', (err, database) => {
-    // start the server, but only when db is connected
-    db = database;
-    app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(err, req) {
-        if(err) {
-            console.log('server error');
-        } else {
-            console.log('server listening on: 3000');
-        }
-    });
-});
+var exphbs = require('express-handlebars');
+var routes = require('./routes/routes');
+var app = express();
 
 // view engine setup
 var hbs = exphbs.create({defaultLayout: 'default',extname: 'hbs', partialsDir: ['views/partials/']});
@@ -28,39 +15,22 @@ app.use(bodyParser.json());
 // set assets path
 app.use(express.static(path.join(__dirname, '/dist')));
 
-// GET req, index
-app.get('/', function (req, res) {
-  // find all items in db collection('name')
-  db.collection('todos').find().toArray((err, result) => {
-      res.render('index', {
-        title: 'To-Do List',
-        todos: result
-      });
-  });
+// use routes
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// GET req, success
-//app.get('/success/:name', function (req, res, next) {
-//    res.render('success', {
-//        title: 'Success',
-//        output: req.params.name
-//    });
-//});
-
-// POST req, save item via form action
-app.post('/submit', (req, res, next) => {
-  db.collection('todos').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-    res.redirect('/');
-    console.log('Item saved.');
-  })
-})
-
-// GET req, delete item
-app.get('/delete/:name', function (req, res) {
-  var name = req.params.name;
-  db.collection('todos').remove({ name }, function(err) {
-    res.redirect( '/' );
-    console.log('Item deleted.');
-  });
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(err, req) {
+  if(err) {
+    console.log('server error');
+    } else {
+      console.log('server listening on: 3000');
+    }
 });
+
+module.exports = app;
---
+++ routes/routes.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var router = express.Router();
+var db;
+
+// Connect to DB
+MongoClient.connect('mongodb://dev:35Xsi6C8*136@ds161640.mlab.com:61640/todolist', (err, database) => {
+    db = database;
+});
+
+// GET items in db collection()
+router.get('/', function (req, res) {
+  db.collection('todos').find().toArray((err, result) => {
+      res.render('index', {
+        title: 'To-Do List',
+        todos: result
+      });
+  });
+});
+
+// GET req, success
+//router.get('/success/:name', function (req, res, next) {
+//    res.render('success', {
+//        title: 'Success',
+//        output: req.params.name
+//    });
+//});
+
+// POST req, save item via form action
+router.post('/create', (req, res, next) => {
+  db.collection('todos').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    res.redirect('/');
+    console.log('Item saved.');
+  })
+})
+
+// GET req, delete item
+router.get('/delete/:name', function (req, res) {
+  var name = req.params.name;
+  db.collection('todos').remove({ name }, function(err) {
+    res.redirect( '/' );
+    console.log('Item deleted.');
+  });
+});
+
+module.exports = router;
