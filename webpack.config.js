77a01d9add90aa253a52f632ea0241755ee84c3b
Brian O'Toole <brian@sparxoo.com>
2017-06-09 11:28:45 -0400

    nodemon + webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
 	entry: './src/js/index.js',
@@ -6,19 +7,25 @@ module.exports = {
 		filename: './dist/js/build.js'
 	},
 	module: {
-		loaders: [
+		rules: [
 			{
 				test: /\.scss$/,
-				loader: ExtractTextPlugin.
+				use: ExtractTextPlugin.
 				    extract({
-				       loader: 'css!sass'
+              fallback: 'style-loader',
+              use: ['css-loader','sass-loader']
 				    })
-				
+
 			},
 		],
 	},
 	plugins: [
-	    new ExtractTextPlugin('./dist/css/style.css')
-	],
-	watch: true,
+	    new ExtractTextPlugin('./dist/css/style.css'),
+      new BrowserSyncPlugin({
+              open: 'external',
+              proxy: 'localhost',
+              port: 3000,
+              files: ['src/**/*.{html,js,css}']
+          })
+	]
 };
